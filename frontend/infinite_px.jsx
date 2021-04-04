8338fb6549282722af9623daf697563f5362ec3b
Cristian Reynoso <cristianreynoso91@gmail.com>
2021-04-04 13:12:52 -0700

    Create basic webpack config

---
+++ frontend/infinite_px.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  ReactDOM.render(<h1>Welcom to the app</h1>, document.getElementById(root))
+})
