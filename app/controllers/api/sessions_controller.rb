8a05daf21c6ce24479d201eef2bdd3f7a3d1bab4
Cristian Reynoso <cristianreynoso91@gmail.com>
2021-04-05 17:14:46 -0700

    Add frontend redux cycle for session and user

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,22 +1,23 @@
 class Api::SessionsController < ApplicationController
   def create
-  @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+    @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
 
-  if @user
-    login(@user)
-    render 'api/users/show'
-  else
-    render json: ["Invalid credentials"], status: 401
+    if @user
+      login(@user)
+      render 'api/users/show'
+    else
+      render json: ["Invalid credentials"], status: 401
+    end
   end
-end
 
-def destroy
-  @user = current_user
-  if @user
-    logout
-    render {}
-  else
-    render json: ["Noone to signout"], status: 404
+  def destroy
+    @user = current_user
+    if @user
+      logout
+      render '/api/users/show'
+    else
+      render json: ["Noone to signout"], status: 404
+    end
   end
-end
+
 end
--- frontend/infinite_px.jsx
+++ frontend/infinite_px.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import * as sessionActions from './actions/session_actions';
+
+import configureStore from './store/store';
+
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
+
+  // Testing
+  window.login = sessionActions.login;
+  window.logout = sessionActions.logout;
+  window.signup = sessionActions.signup;
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  // Testing
+
   ReactDOM.render(<h1>Welcome to InfinitePX</h1>, document.getElementById('root'))
-})
+});
