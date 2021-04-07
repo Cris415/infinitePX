a709c5c53ccdbf1a203abc4ac8b90339dceac935
Cristian Reynoso <cristianreynoso91@gmail.com>
2021-04-07 12:26:14 -0700

    Add posts controller, index show and create methods

---
+++ app/controllers/api/posts_controller.rb
@@ -0,0 +1,26 @@
+class Api::PostsController < ApplicationController
+  before_action :require_login, only: [:create]
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def create
+    @post = Post.new(post_params)
+    
+    if @post.save
+      render :show
+    else
+      render @post.errors.full_messages, status: 400
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :description, :user_id)
+  end 
+end
