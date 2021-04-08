f2333315c0d42cc5663c72e620fb0fe2cd426499
Cristian Reynoso <cristianreynoso91@gmail.com>
2021-04-08 11:08:12 -0700

    Change users reducer to not replace current state so that logged in user would not be removed if they had not created a post

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -5,7 +5,7 @@ const usersReducer = (state={}, action) => {
   Object.freeze(state);
   switch (action.type) {
     case RECEIVE_POSTS:
-      return Object.assign({}, action.users );
+      return Object.assign({}, action.users, state );
     case RECEIVE_POST:
       return Object.assign({}, state, { [action.user.id]: action.user });
     case RECEIVE_CURRENT_USER:
