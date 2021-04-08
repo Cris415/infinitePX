7b3d969193aaae94d7d3d7aa4a23861f3bb5df1d
Cristian Reynoso <cristianreynoso91@gmail.com>
2021-04-08 16:01:43 -0700

    Changes related to active storage setup

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,11 @@
 class Post < ApplicationRecord
-  validates :description, :title, :user_id, presence:true
+  validates :description, :title, :user_id, presence: true
   validates :title, uniqueness: { scope: :user_id, message: "Cannot create identical titles" }
 
   belongs_to :user,
   foreign_key: :user_id,
   class_name: :User
+
+  has_one_attached :photo
 
 end
