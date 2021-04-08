1b50f00eefd1c16b047627c9d3221434bb3dd24a
Cristian Reynoso <cristianreynoso91@gmail.com>
2021-04-08 11:57:15 -0700

    Add basic styling to feed and create a header class

--- frontend/components/feed.jsx
+++ frontend/components/feed.jsx
@@ -4,8 +4,10 @@ import PostIndexContainer from './posts/post_index_container';
 const Feed = () => {
   return (
     <div className="feed" >
-      <h1>Home Feed</h1>
-      <p>See photos from other photographers</p>
+      <div className="header">
+        <h1>Home Feed</h1>
+        <p>See photos from other photographers</p>
+      </div>
 
       <PostIndexContainer />
     </div>
--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Splash = () => {
   return (
     <div className="splash">
+      <div className="header">
       <h1>Welcome to infinitePx</h1>
+      </div>
     </div>
   );
 }
