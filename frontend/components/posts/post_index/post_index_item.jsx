b34ccfb912d214f7a98a9f3f8dd92e9e65c4609d
Cristian Reynoso <cristianreynoso91@gmail.com>
2021-04-13 18:21:21 -0700

    Refactor styling for post index item

--- frontend/components/posts/post_index/post_index_item.jsx
+++ frontend/components/posts/post_index/post_index_item.jsx
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom';
 const PostIndexItem = ({post, author}) => {
   return (
     <li className="index-item">
-      <p className="index-item-top">{post.title}</p>
+      <p className="index-item-heading index-item-top">{post.title}</p>
 
       <Link className="index-item-image-link" to={`/posts/${post.id}`}>
         <img src={post.photoUrl} alt={post.title} />
       </Link>
 
       <Link
-        className="index-item-bottom"
+        className="index-item-heading index-item-bottom"
         to={`/users/${post.userId}`}
       >
         <p>{author}</p>
