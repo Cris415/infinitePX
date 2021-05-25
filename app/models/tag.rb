2da968068d460d62f25d27d96570f78881ceb4de
Cristian Reynoso <cristianreynoso91@gmail.com>
2021-05-25 10:52:51 -0700

    create model for tags and taggedPosts

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,12 @@
 class Tag < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+
+  has_many :tagged_posts,
+    foreign_key: :tag_id,
+    class_name: :TaggedPost
+
+  has_many :posts,
+    through: :tagged_posts,
+    source: :post
+
 end
--- app/models/tagged_post.rb
+++ app/models/tagged_post.rb
@@ -1,2 +1,12 @@
 class TaggedPost < ApplicationRecord
+  validates :post_id, uniqueness: { scope: :tag_id, messages: "cannot tag the same post twice with the same tag"}
+
+  belongs_to :post,
+  foreign_key: :post_id,
+  class_name: :Post
+
+  belongs_to :tag,
+  foreign_key: :tag_id,
+  class_name: :Tag
+
 end
