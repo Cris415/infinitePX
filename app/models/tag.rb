feb7288bca5d2a6ac0671c72b4d1072c36675291
Cristian Reynoso <cristianreynoso91@gmail.com>
2021-05-25 18:22:40 -0700

    add tag length limit

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,12 @@
 class Tag < ApplicationRecord
   validates :name, presence: true, uniqueness: true
+  validate  :tag_too_long
+
+  def tag_too_long
+    if name && name.length > 30 
+      errors.add :name, message:  'is too long'
+    end
+  end
 
   has_many :tagged_posts,
     foreign_key: :tag_id,
