173282034a51efdae10f62d3e8594f57482cdcf2
Cristian Reynoso <cristianreynoso91@gmail.com>
2021-06-10 11:36:13 -0700

    Add new tag seeds and refactor tag portion of seed file

---
+++ db/tag_names.rb
@@ -0,0 +1,132 @@
+module Tags
+  Names =
+    %w(architecture activity adorable abstract calm adult adventure areal
+       affection africa agriculture air alaska alberta alone alpine alps
+       alternative amazing america animal antique arch arches artic
+       architectural artic area argentina art asia asian asking atlantic
+       atmosphere attraction attractive aurora autumn before\ and\ after
+       boat boats body bokeh basketball botanical botany botswana box boy
+       branch branches black\ and\ white b&w brasil brazil breakfast brick
+       bridge bright british brooklyn brother brown brunette bud budapest
+       buddha buddhism bug building bush business business\ people
+       business\ man business\ woman businessman businesswoman butterfly
+       brilliant cactus calgary california campting canada canal candid
+       canine canyon capital car caribbean cascade castle casual
+       casual\ clothing cat cathedral caucasian cave celebration cellphone chart
+       cheerful cheetah cherry chicago child childhood children chile
+       china christmas church city cityscape classroom clean clear cliff
+       close-up closeup close\ up clouds cloud cloudscape cloudy coast
+       coastal coastline cocktail coffee cold collaboration color
+       color\ image colorful colors colour colourful communication
+       composition computer concept connection construction contrast
+       convenience cooking cool copy\ space corn country countryside
+       couple cow creative creature creek crop cub cubs culture currency
+       customer\ service cure cycling dubai duck dunes daisy dancing
+       dandelion danger dark dawn day daylight daytime decay decoration
+       deer delicate delicious depression desert design desk
+       dessert destination detail dew digital digital\ manipulation
+       digital\ tablet dock doctor dog dolomites dolomiti dome domestic
+       domestic\ animal door downtown dragonfly dramatic dream dress
+       details drink drop dry eagle early earth eat eating ecology
+       education elegance elegant elephant embrace empty endangered energy
+       engineer entertainment environment erosion estonia europe evening
+       exercise exercising exotic exploration exterior eye eyes fireworks
+       fish fishing five face fall falls family fantasy farm farmer
+       farming farmland fashion fashion\ model fauna feather feeding
+       feline female fence flag field fields film filter fine\ art fire
+       fireworks fish fishing five flamingo flight floral flow flower
+       flowers flowing fluffy fly flying focus fog foggy foliage food forest
+       formation forrest four fox france freedom freeze fresh freshness
+       friends friendship frog focused front\ view frost frozen fruit
+       full\ length fun funny fur furniture gal garden gate geology
+       germany gift giraffe girl giving glacier glamour glass globe glow
+       golden golden\ hour gorge grain grand\ canyon grandchild grandmother
+       grandparent grass grassland gray great green grey grin groceries
+       ground group growth gym hair hand hands handshake happiness happy
+       harbor harbour harvest hawaii hawk haze hdr head headphones headset
+       health health\ club health\ spa healthy heaven heritage heron high
+       high\ angle highlands highway hike hiking hill himalaya himalayas
+       hispanic historic historical hokkaido holding holiday holland home
+       hong\ kong horizon horizontal horse horses hot hotel house hug human
+       human\ being hummingbird hungary hunter hunting ice iceberg iceland
+       idyllic illuminated indian indoor indoors industrial industry
+       ingredient insect insects inside instructor interior internet
+       island isolated istanbul italian italy japanese jetty jogging
+       journey joy jumping jungle juvenile kid kingfisher kiss kitchen
+       kitten kitty laboratory lady lagoon lake land landmark lanscape
+       landscapes laptop large large laughing lava lavender leaf leaves
+       legs leisure lens leopard library life lifestyle lifestyles
+       lighthouse lightning lights lily lines line lion little lizard
+       london lonely long long\ exposure looking looking\ down lotus
+       love lovely lush macro magic main majestic make-up male mammal man
+       manhattan map mare marina marine marsh meadow medical medieval
+       mediterranean meeting mexico milky\ way mill milk mirror mist
+       misty moab mobile mobile\ phone mobility model modern monkey
+       mono monochrome montana monument mood moody moon morning morocoo
+       moscow mosque moss mother motion motorcycle mount mountain
+       mountains movement moving\ up mushroom music national national\ park
+       natural natural\ light natural\ world nature nature\ photograph
+       nature\ pics negative\ space nepal nest nevada new\ york
+       night nightclub nightscape nobody no\ people north north\ carolina
+       northern\ lights norway nude nuse nyc ocean office old one
+       one\ person online orange outdoor outdoors outside overcast
+       overhead owl pacific palace palm panorama panoramic paradise
+       parent paris park party pasture patagonia path patient pattern
+       peace peaceful peacock peak peaks pelican people perch perched
+       person perspective peru pet petal photo photography picture pier
+       pine pink place places plant plants play playa plumage pollen
+       pond pool poppy port portrait posing power prague predator
+       presentation pretty prey primate puddle puppu pure purple quiet
+       rain rainbow rainforest range raptor raw ray reading real\ people
+       rear\ view recreation red red\ fox reflection reflections
+       relationship relax relaxation religion repairing reptile reserve
+       resort rest restaurant resting retro rice ridge river road rock
+       rockies rock rocky rocky\ mountains romania romantic rome roof
+       rose ruins running rural rust rustic sadness safari san\ francisco
+       sand sandstone santorini scandinavia scene scenery scenic scenics
+       school sharp sculpture sea seagull seascape seashore seaside
+       seasons seattle see seminar senior sensual sensuality sepia serene
+       serenity series sexy shadow shadows sharing sheep ship shine
+       shopping shore shoreline sibling sightseeing silence silhouette
+       singapore single sisters sitting ski skiing skin sky skyline
+       skyscraper sleeping small small\ business smart\ phone smile
+       smiling smoke smooth snail snake snow soft solitude south spa
+       space spider splash sport spring springtime square quirrel
+       stairs standing star stars startup statue stem still still\ life
+       stone stones store storm stream street structure student studio
+       style suburban summer summit sun sundown sunflower sunlight sunny
+       sunrise sunset sunshine surf surreal swamp swan sweet swimming
+       sydney symbol table tablet tail taiwan talking tall tasty
+       teacher technology teenager telephone television temple texas
+       text texture thai thailand three tide tiger time together
+       togetherness top toronto torino tour tourism tourist tower town
+       track traditional traffic trail train training tranquil
+       tranquility transportation travel tree trees trekking trip
+       tropical trunk tulip tulips turkey turquoise turtle tuscany
+       twilight two ukraine underwater unesco united\ kingdom
+       university up urban ubex usa using\ computer vacation valley
+       vancouver vegetable vegetation vehicle venice vertical
+       veterinarian vibrant victoria vietnam view view\ from\ above
+       village vintage violet vista vivid volcano volcanic walk walking
+       wall wallpaper warm washington water waterfall waterfront wave way
+       weather web wedding west wet wheat white white\ space wide\ angle
+       wild wild\ animal wilderness wildlife wind windmill window wine
+       wing winter wireless wolf wolves woman women wood wooden
+       woodland woods work working world worried yellow yellowstone
+       yoga yosemite young young\ adult young\ men young\ women youth
+       yukon zebra zion zo vibes iconic destination mood vacay
+       perspectives view unfiltered angles inspiration views landscape
+       portrait process panoramic panorama skyline cityscape nature
+       natural  rural life wonders wonder still\ life food
+      skyscraper bridge rainforest jungle sunrise dawn dusk
+       sunset stadium mugshot filtered garden lake river ocean beach
+       flower love scene scenic attitude altitude snow rain sun moon
+       stars sky green art sculpture museum office library street church
+       concert home house hotel joy earth toys breakfast lunch
+       dinner snack desert editing headshot cover professional amateur
+       sponsored free boy girl baby daddy dad mother father brother
+       sister daughter son mom mommy pets troublemaker trouble
+       perception scenes peek glance photograph moments moment icon
+       artistic artist showstopper showstopping
+    ).uniq
+end
