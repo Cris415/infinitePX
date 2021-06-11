c29472b4780d0f4273ec91808116e12685b6b763
Cristian Reynoso <cristianreynoso91@gmail.com>
2021-06-11 16:27:24 -0700

    Update splash image and tweak styling

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -7,6 +7,21 @@ const Footer = () => {
       <a href="https://github.com/Cris415/infinitePX" target="_blank">
         github
       </a>
+      <a href="https://github.com/Cris415" target="_blank" rel="noreferrer">
+        , Cristian:{" "}
+      </a>
+      <div>
+        <a href="https://github.com/Cris415" target="_blank" rel="noreferrer">
+          <img src="GitHub-Mark-Light-64px.png" alt="github-img" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/cristian-reynoso-31b4168b/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src="linkedin.png" alt="linked-img" />
+        </a>
+      </div>
     </footer>
   );
 }
--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -5,10 +5,24 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
 const Splash = () => {
+  const splashImgIdx = Math.floor(Math.random() * 10) > 1 ? 1 : 0;
   return (
     <div className="splash">
       <div className="splash-hero">
-        <img src={window.splash} alt="hero image flower" />
+        <img src={window[`splash${splashImgIdx}`]} alt="hero-image" />
+        <div className="custom-shape-divider-bottom-1623443712">
+          <svg
+            data-name="Layer 1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1200 120"
+            preserveAspectRatio="none"
+          >
+            <path
+              d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+              className="shape-fill"
+            ></path>
+          </svg>
+        </div>
         <div className="splash-hero-copy">
           <h1>Welcome to InfinitePx</h1>
           <p>
