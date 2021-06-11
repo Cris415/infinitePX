ec8b2fa5b8307c1fb26df9fc4bef530abbfee789
Cristian Reynoso <cristianreynoso91@gmail.com>
2021-06-11 16:58:10 -0700

    Update footer links

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -1,29 +1,29 @@
-import React from 'react';  
+import React from "react";
 
 const Footer = () => {
   return (
     <footer className="footer">
-      InfinitePx
-      <a href="https://github.com/Cris415/infinitePX" target="_blank">
-        github
+      <a
+        href="https://github.com/Cris415/infinitePX"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <span className="footer-logo">InfinitePx</span>
       </a>
-      <a href="https://github.com/Cris415" target="_blank" rel="noreferrer">
-        , Cristian:{" "}
-      </a>
-      <div>
+      <div className='footer-links'>
         <a href="https://github.com/Cris415" target="_blank" rel="noreferrer">
-          <img src="GitHub-Mark-Light-64px.png" alt="github-img" />
+          <img src={window.github_logo} alt="github-img" />
         </a>
         <a
           href="https://www.linkedin.com/in/cristian-reynoso-31b4168b/"
           target="_blank"
           rel="noreferrer"
         >
-          <img src="linkedin.png" alt="linked-img" />
+          <img src={window.linkedin_logo} alt="linked-img" />
         </a>
       </div>
     </footer>
   );
-}
+};
 
 export default Footer;
