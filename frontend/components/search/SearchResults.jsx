70b0f5bb0a12b790558dec73bce5141656b1d084
Cristian Reynoso <cristianreynoso91@gmail.com>
2021-06-12 07:56:36 -0700

    Update searchResults to search when component mounts

--- frontend/components/search/SearchResults.jsx
+++ frontend/components/search/SearchResults.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 import { connect } from "react-redux";
 import PostIndex from "../posts/post_index/post_index";
+import { searchPosts } from '../../actions/post_actions';
 import SearchPageBarContainer from "./SearchPageBarContainer";
 
-function SearchResults(props) {
-  return (
-    <div>
-      <SearchPageBarContainer searchTerm={props.searchTerm} />
-      <PostIndex {...props} />
-    </div>
-  );
+class SearchResults extends React.Component {
+  constructor(props){
+    super(props)
+  } 
+  componentDidMount(){
+    this.props.search();
+  }
+
+  render(){
+    return (
+      <div>
+        <SearchPageBarContainer searchTerm={this.props.searchTerm} />
+        <PostIndex {...this.props} />
+      </div>
+    );
+  }
+
 }
 
 const mapStateToProps = (state, ownProps) => ({
@@ -20,4 +31,9 @@ const mapStateToProps = (state, ownProps) => ({
   searchTerm: ownProps.location.pathname.split("/")[2],
 });
 
-export default connect(mapStateToProps)(SearchResults);
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  search: () =>
+    dispatch(searchPosts(ownProps.location.pathname.split("/")[2])),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(SearchResults);
