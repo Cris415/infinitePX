7d24de819afa961a1f5a4df40b030d03589c5204
Cristian Reynoso <cristianreynoso91@gmail.com>
2022-01-26 12:19:53 -0800

    Filter tags for post and destructure long variables

--- frontend/components/posts/post_show/post_show_container.js
+++ frontend/components/posts/post_show/post_show_container.js
@@ -1,21 +1,35 @@
-import { connect } from 'react-redux';
-import PostShow from './post_show';
-import { fetchPost, searchPosts } from '../../../actions/post_actions'
+import { connect } from "react-redux";
+import PostShow from "./post_show";
+import { fetchPost, searchPosts } from "../../../actions/post_actions";
 
-import { postAuthorSelector } from '../../../selectors/post_author_selector';
+import { postAuthorSelector } from "../../../selectors/post_author_selector";
 
-const mapStateToProps = (state, ownProps) => ({
-  post: state.entities.posts[ownProps.match.params.postId],
-  author: postAuthorSelector(state, ownProps.match.params.postId),
-  tags: Object.values(state.entities.tags),
-  currentUserId: state.session.id,
-  errors: state.errors.posts,
-  loading: state.ui.loading
-});
+const mapStateToProps = (state, ownProps) => {
+  const { posts, tags } = state.entities;
+  const { postId } = ownProps.match.params;
+  let postTags = [];
+
+  if (posts[postId]) {
+    postTags = posts[postId].tagIds;
+  }
+
+  const newTags = Object.values(tags).filter((tag) =>
+    postTags.includes(tag.id)
+  );
+
+  return {
+    post: posts[postId],
+    author: postAuthorSelector(state, postId),
+    tags: newTags,
+    currentUserId: state.session.id,
+    errors: state.errors.posts,
+    loading: state.ui.loading,
+  };
+};
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
-    fetchPost: () => dispatch(fetchPost(ownProps.match.params.postId)),
-    searchPosts: (searchTerm) => dispatch(searchPosts(searchTerm, 'tag'))
-  })
+  fetchPost: () => dispatch(fetchPost(ownProps.match.params.postId)),
+  searchPosts: (searchTerm) => dispatch(searchPosts(searchTerm, "tag")),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(PostShow);
