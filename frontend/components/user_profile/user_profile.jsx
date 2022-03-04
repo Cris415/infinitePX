2b6cbd511d843eebe5cf4a66183a996fdd3fc7d3
Cristian Reynoso <cristianreynoso91@gmail.com>
2022-03-03 17:49:36 -0800

    Refactor component to functional component

--- frontend/components/user_profile/user_profile.jsx
+++ frontend/components/user_profile/user_profile.jsx
@@ -1,46 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PostIndex from "../posts/post_index/post_index";
 import UserIcon from "./user_icon";
 
 import FollowButtonContainer from "../follow_button/follow_button_container";
 
-class UserProfile extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+function UserProfile(props) {
+  const { user, posts, currentUserId, fetchUserPosts, fetchUser } = props;
 
-  componentDidMount() {
-    this.props.fetchUserPosts();
-    this.props.fetchUser();
-  }
+  useEffect(() => {
+    fetchUserPosts();
+    fetchUser();
+  }, []);
 
-  render() {
-    if (!this.props.user) return null;
-    const user = { [this.props.user.id]: this.props.user };
-    return (
-      <div className="user-profile">
-        <div className="header user-profile-header">
-          <UserIcon size="large" />
-          <h3 className="user-profile-username">{this.props.user.username}</h3>
-          <FollowButtonContainer user={this.props.user} />
-          <div className="user-profile-count">
-            <span>
-              {this.props.user.followersCount} <strong>Followers</strong>
-            </span>
-            <span>
-              {this.props.user.follows.length} <strong>Following</strong>
-            </span>
-          </div>
+  if (!user) return null;
+  const userForPostIndex = { [user.id]: user };
+  return (
+    <div className="user-profile">
+      <div className="header user-profile-header">
+        <UserIcon size="large" />
+        <h3 className="user-profile-username">{user.username}</h3>
+        <FollowButtonContainer user={user} />
+        <div className="user-profile-count">
+          <span>
+            {user.followersCount} <strong>Followers</strong>
+          </span>
+          <span>
+            {user.follows.length} <strong>Following</strong>
+          </span>
         </div>
-        <PostIndex
-          posts={this.props.posts}
-          users={user}
-          indexType="userIndex"
-          currentUserId={this.props.currentUserId}
-        />
       </div>
-    );
-  }
+      
+      <PostIndex
+        posts={posts}
+        users={userForPostIndex}
+        indexType="userIndex"
+        currentUserId={currentUserId}
+      />
+    </div>
+  );
 }
 
 export default UserProfile;
